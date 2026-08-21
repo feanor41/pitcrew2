99cd3b0daad12abdd7669d821774bc622e3c3516
Federico Mazzalomo <federicomazzalomo@gmail.com>
2026-08-21 19:43:28 -0300

    feat(cli): expose embedded tui

--- cmd/pitcrew/main.go
+++ cmd/pitcrew/main.go
@@ -15,9 +15,9 @@ func main() {
 	if err != nil {
 		root = "."
 	}
-	os.Exit(run(os.Args[1:], os.Stdout, os.Stderr, root))
+	os.Exit(run(os.Args[1:], os.Stdin, os.Stdout, os.Stderr, root))
 }
 
-func run(args []string, stdout, stderr io.Writer, root string) int {
-	return cli.Run(args, cli.Dependencies{Stdout: stdout, Stderr: stderr, ProjectRoot: root, Version: version, Now: time.Now})
+func run(args []string, stdin io.Reader, stdout, stderr io.Writer, root string) int {
+	return cli.Run(args, cli.Dependencies{Stdin: stdin, Stdout: stdout, Stderr: stderr, ProjectRoot: root, Version: version, Now: time.Now})
 }
